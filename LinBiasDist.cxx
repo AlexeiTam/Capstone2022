7036c1d0b31c16c065e0887fe06092902c278b1d
AlexeiTam <ttam5@LAPTOP-U98AKGCO.localdomain>
2022-11-01 08:54:23 -0500

    Add variables, constants, hist and func

--- LinBiasDist.cxx
+++ LinBiasDist.cxx
@@ -2,4 +2,29 @@ void LinBiasDist(){
 
 	//GOAL:bias a uniform distribution to make a linear-looking function
 
+	//initializing variables, constants
+	int NEvents = 1000;
+	int NBins = 100;
+
+	float xmin = 0.0;	//give range of x
+	float xmax = 10.0;
+
+	float m = 1.0;	//linear slope
+	float b = 0.0;	//y-intercept
+	
+	//linear function
+	TF1* f1 = new TF1("fit","[0]*x + [1]", xmin, xmax);
+	f1->SetParameters(m,b);
+
+	//histogram
+	TH1D h1 = new TH1D("h1","", NBins, xmin, xmax);
+
+	float xRight[NBins];
+
+	//finding right edges of bins
+	for(int i = 0; i < NBins; i++){
+	
+		xRight[i] = (i+1)*((xmax - xmin)/(NBins));
+		std::cout << xRight[i] << std::endl;
+	}
 }

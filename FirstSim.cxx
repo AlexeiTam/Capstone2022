463369d0ef4ad3c5ca1f2a9809a06d0a0be6f122
AlexeiTam <ttam5@LAPTOP-U98AKGCO.localdomain>
2023-02-13 09:53:03 -0600

    general update

--- FirstSim.cxx
+++ FirstSim.cxx
@@ -37,10 +37,13 @@ void FirstSim() {
 	float mC[NEvents];	//mC1 = mC2	//for simplicity
 	float EC1[NEvents];
 	float EC2[NEvents];
+
+	//!!Move mC generation here
+	//
 		//beta-->constrained: [0,1]	//for simplicity
 	for(int i = 0; i < NEvents; i++) {
 
-		betaC[i] = gRandom->Rndm();
+		betaC[i] = gRandom->Rndm();	//fix: constrained by E^2 = p^2 + m^2
 		gammaC[i] = sqrt((1.0)/(1.0 - (betaC[i])*(betaC[i])));
 		mC[i] = gRandom->Gaus(0.5*mPMean, 0.5*mPSigma);	
 	}
@@ -58,26 +61,110 @@ void FirstSim() {
 		pC2z[i] = 0.0;
 	}
 
-		//CHOOSE z-direction s.t. pC1 and PC2 are purely in z-axis
+		//CHOOSE x-direction s.t. pC1 and PC2 are purely in x-axis
 		//(?)p = gamma*mv = gamma*mc*beta
 		//!!ED: generate p, let E be determined by constraint
 	for(int i = 0; i < NEvents; i++) {
 
-		pC1z[i] = gRandom->Rndm();
-		pC2z[i] = -1.0*pC1z[i];
+		pC1x[i] = gRandom->Rndm();//!!E^2 = p^2 + m^2
+		pC2x[i] = -1.0*pC1x[i];
 	}
 
 		//constraint: E(i)^2 = p(i)^2c^2 + m(i)^2c^4
 
 	for(int i = 0; i < NEvents; i++) {
 
-		EC1[i] = sqrt((pC1z[i])*(pC1z[i])*(c*c) + (mC[i])*(mC[i])*(c*c*c*c) );	//|p| = pz ; as px = py = 0
-		EC2[i] = sqrt((pC2z[i])*(pC2z[i])*(c*c) + (mC[i])*(mC[i])*(c*c*c*c) );	//|p| = pz ; as px = py = 0
+		EC1[i] = sqrt((pC1x[i])*(pC1x[i])*(c*c) + (mC[i])*(mC[i])*(c*c*c*c) );	//|p| = pz ; as px = py = 0
+		EC2[i] = sqrt((pC2x[i])*(pC2x[i])*(c*c) + (mC[i])*(mC[i])*(c*c*c*c) );	//|p| = pz ; as px = py = 0
 	}
 
 
 		//Generate L[NEvents][rows][columns]
-		//
+		//and momentum 4-vectors P[NEvents][rows]
+	float L[NEvents][4][4];
+	float PC1[NEvents][4];
+	float PC2[NEvents][4];
+
+	//initialize as 0 matrix
+	for(int i = 0; i < NEvents; i++) { //Event Scan
+
+		for(int j = 0; j < 4; j++) {
+
+			for(int k = 0; k < 4; k++) {
+
+				L[i][j][k] = 0.0;
+			}
+		}
+
+	}	
+
+
+	//fill with appropriate gamma, beta values for each event
+	//
+	for(int i = 0; i < NEvents; i++) {
+
+		L[i][0][0] = gammaC[i];
+		L[i][1][1] = gammaC[i];
+		L[i][0][1] = -1.0*(gammaC[i])*(betaC[i]);
+		L[i][1][0] = -1.0*(gammaC[i])*(betaC[i]);
+
+		L[i][2][2] = 1.0;
+		L[i][3][3] = 1.0;
+	}
+
+	//fill P-vectors:
+	//
+	for(int i = 0; i < NEvents; i++) {
+
+		PC1[i][0] = EC1[i];
+		PC1[i][1] = pC1x[i];
+		PC1[i][2] = pC1y[i];
+		PC1[i][3] = pC1z[i];
+
+		PC2[i][0] = EC2[i];
+		PC2[i][1] = pC2x[i];
+		PC2[i][2] = pC2y[i];
+		PC2[i][3] = pC2z[i];
+	}
+
+//BOOST: P->C1
+
+	float PCprime[NEvents][4];	//PCprime = PC1 in RF(C1)
 	
+	//initialize as 0
+	for(int i = 0; i < NEvents; i++) {
+
+		for(int j = 0; j < 4; j++) {
+
+			PCprime[i][j] = 0.0;
+		}
+	}
+
+	//perform LT:
+	for(int k = 0; k < NEvents; k++) {
+		
+		for(int i = 0 ; i < 4; i ++) {
+
+			for(int j = 0; j < 4; j++) {
+
+				PCprime[k][i] = PCprime[k][i] + (L[k][i][j])*PC1[k][j];
+			}
+		}
+		
+	}
+
+
+
+//RF(C1):
+//
+
+	//CHECK::
+	//
+	const Int_t NBins = 1000;
+	float xmin = mPMean - (5.0*mPSigma); 
+	float xmax = mPMean + (5.0*mPSigma); 
+	TH1D *hmP = new TH1D("hmP","mP Distribution", NBins, xmin, xmax);
+
+		
 
 }

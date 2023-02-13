bac29069a6074b644fce6b66b5694413936a616b
AlexeiTam <ttam5@LAPTOP-U98AKGCO.localdomain>
2023-02-12 20:44:57 -0600

    update; generate boost for RF(P) -> RF(C1)

--- FirstSim.cxx
+++ FirstSim.cxx
@@ -35,13 +35,14 @@ void FirstSim() {
 	float pC2z[NEvents];
 
 	float mC[NEvents];	//mC1 = mC2	//for simplicity
-	
+	float EC1[NEvents];
+	float EC2[NEvents];
 		//beta-->constrained: [0,1]	//for simplicity
 	for(int i = 0; i < NEvents; i++) {
 
 		betaC[i] = gRandom->Rndm();
 		gammaC[i] = sqrt((1.0)/(1.0 - (betaC[i])*(betaC[i])));
-
+		mC[i] = gRandom->Gaus(0.5*mPMean, 0.5*mPSigma);	
 	}
 
 		//generating pC1, pC2 with pC1 = -pC2
@@ -58,10 +59,25 @@ void FirstSim() {
 	}
 
 		//CHOOSE z-direction s.t. pC1 and PC2 are purely in z-axis
+		//(?)p = gamma*mv = gamma*mc*beta
+		//!!ED: generate p, let E be determined by constraint
 	for(int i = 0; i < NEvents; i++) {
 
-		pC1z[i] = 
+		pC1z[i] = gRandom->Rndm();
+		pC2z[i] = -1.0*pC1z[i];
 	}
 
+		//constraint: E(i)^2 = p(i)^2c^2 + m(i)^2c^4
+
+	for(int i = 0; i < NEvents; i++) {
+
+		EC1[i] = sqrt((pC1z[i])*(pC1z[i])*(c*c) + (mC[i])*(mC[i])*(c*c*c*c) );	//|p| = pz ; as px = py = 0
+		EC2[i] = sqrt((pC2z[i])*(pC2z[i])*(c*c) + (mC[i])*(mC[i])*(c*c*c*c) );	//|p| = pz ; as px = py = 0
+	}
+
+
+		//Generate L[NEvents][rows][columns]
+		//
+	
 
 }

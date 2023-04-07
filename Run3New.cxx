d9aa3a0f947552e8f2428068237c5adbf867a95f
AlexeiTam <107886014+AlexeiTam@users.noreply.github.com>
2023-04-06 21:59:21 -0500

    try to fix hists again

--- Run3New.cxx
+++ Run3New.cxx
@@ -530,11 +530,11 @@ void Run3New(){
 	//
 
 	TH1D *hmCNew = new TH1D("hmCNew","X17 Mass (RF:X17)", NBins,(*min_element(mCNew,mCNew+NEvents)) - 10.0, (*max_element(mCNew,mCNew+NEvents)) + 10.0 );
-	TH1D *hECNew = new TH1D("hECNew","X17 Energy (RF:C1)", NBins,(*min_element(ECNew,ECNew+NEvents)) - 10.0, (*max_element(ECNew,ECNew+NEvents)) + 10.0 );
-	TH1D *hmG11 = new TH1D("hmG11","e^{+},e^{-} Mass", NBins, (*min_element(mG11,mG11+NEvents)) - 10.0, (*max_element(mG11,mG11+NEvents)) + 10.0 );
+	TH1D *hECNew = new TH1D("hECNew","X17 Energy (RF:C1)", NBins, -10.0, (*max_element(ECNew,ECNew+NEvents)) + 10.0 );
+	TH1D *hmG11 = new TH1D("hmG11","e^{+},e^{-} Mass", NBins, -10.0, (*max_element(mG11,mG11+NEvents)) + 10.0 );
 	//TH1D *hmC2 = new TH1D("hmC1","C2 Mass", NBins, (*min_element(vmC2.begin(), vmC1.end())) - 5.0; (*max_element(vmC2.begin(), vmC1.end())) + 5.0);
-	TH1D *hEG11 = new TH1D("hEG11","e^{+},e^{-} Energy", NBins, (*min_element(EG11,EG11+NEvents)) - 10.0, (*max_element(EG11,EG11+NEvents)) + 10.0 );
-	TH1D *hpG11 = new TH1D("hpG11","e^{+},e^{-} Momentum", NBins, (*min_element(pG11y,pG11y+NEvents)) - 10.0, (*max_element(pG11y,pG11y+NEvents)) + 10.0 );
+	TH1D *hEG11 = new TH1D("hEG11","e^{+},e^{-} Energy", NBins, -10.0, (*max_element(EG11,EG11+NEvents)) + 10.0 );
+	TH1D *hpG11 = new TH1D("hpG11","e^{+},e^{-} Momentum", NBins, (*min_element(pG11y,pG11y+NEvents))-10.0, (*max_element(pG11y,pG11y+NEvents)) + 10.0 );
 	//TH1D *hpC2 = new TH1D("hpC2","Parent Mass Distribution", NBins, mPmin - 5.0; mPmax + 5.0);
 
 	TH1D *hTheta = new TH1D("hTheta", "Angular Deflection", NBins, -5.0, 185.0);

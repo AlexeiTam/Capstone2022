8f87a457e75ca218e8b1f08991832dd6d40e7eda
AlexeiTam <ttam5@LAPTOP-U98AKGCO.localdomain>
2023-04-08 19:00:20 -0500

    commit just to be sure

--- HistBoundaryTest.cxx
+++ HistBoundaryTest.cxx
@@ -1,18 +1,30 @@
 void HistBoundaryTest(){
 
+	float v1min = -0.0;
+	float v1max = 0.0;
 	std::vector<float> v1;
+	
+	TH1D *hmP = new TH1D("hmP","^{8}Be^{*} Mass", NBins, v1min, v1max);
+	
 	for(int i = 0; i < NEvents; i++){
 		
-		v1.emplace_back(i*2);
+		v1.emplace_back(gRandom->Rndm());
+		if(v1.at(i) > v1max){
+			v1max = v1.at(i);
+		}
 
-	}	
+		cout << v1.at(i) << "..." << v1max << endl;
+	}
 
 	int index = 0;
 
 	while(index < v1.Size()){
 
+		hmP->Fill(v1.at(index));
 		cout << v1.at(index) << endl;
 	}
 
+	hmP->Update();
+	hmP->Draw();
 
 }

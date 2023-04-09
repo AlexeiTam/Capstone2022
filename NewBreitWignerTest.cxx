b34119ddb0eef2f2c55c0f5b6d9838402895d0d5
AlexeiTam <107886014+AlexeiTam@users.noreply.github.com>
2023-04-09 17:22:22 -0500

    fix mC1Max

--- NewBreitWignerTest.cxx
+++ NewBreitWignerTest.cxx
@@ -6,7 +6,7 @@ float pi = TMath::Pi();
   float cutoff = 0.10;
 
 float mC1Norm = (2*mC1Sigma*mC1Sigma)/((fabs(mC1Sigma))*(fabs(mC1Sigma))*(fabs(mC1Sigma)));
-	mC1Max = mC2Mean + (sqrt(((mC1Norm*mC1Sigma)/(pi*cutoff))-(mC1Sigma*mC1Sigma)));
+float mC1Max = mC1Mean + (sqrt(((mC1Norm*mC1Sigma)/(pi*cutoff))-(mC1Sigma*mC1Sigma)));
 
 		TF1 *BWC1 = new TF1("BWC1","((([3])/[0])*(([2])/(((x-[1])*(x-[1]))+([2]*[2]))))", 0.0, mC1Max);
 		BWC1->SetParameters(pi, mC1Mean, mC1Sigma, mC1Norm);

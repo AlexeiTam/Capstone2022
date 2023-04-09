cef6249827919180b11a47b652ce6c9bfc56c8a8
AlexeiTam <107886014+AlexeiTam@users.noreply.github.com>
2023-04-08 20:40:34 -0500

    more progress on X17Simulation.cxx, ; begin generating P, then everything else

--- X17Simulation.cxx
+++ X17Simulation.cxx
@@ -8,6 +8,18 @@ const Int_t NBins = 1000;	//for histograms
 float pi = TMath::Pi();
 float c = 1.0;
   
+    //mass generation parameters
+    float mPMean = 8023.15;		//8Be* mass (MeV)
+	  float mPSigma = 3.9E-11;	//8Be* stable
+
+	  float mC2Mean = 8005.30510;	//given base value of 8Be isotope
+	  float mC2Sigma = 3.9E-11;	//8Be also stable
+
+	  float mC1Mean = 16.98;	//X17 ==>invariant mass approx. 17 MeV
+	  float mC1Sigma = 1.0; 	//will play with later?
+
+	  float me = 0.511;	//mass of electron/positron = 0.511 MeV
+  
 //================================ REST FRAME: 8Be* ================================
   
   float mP[N], mC2[N];
@@ -37,6 +49,26 @@ float c = 1.0;
   float dot[N], cosine[N], pG11New[N], pG11New[N], pG12New[N];
   float mTotal[N], pTotal[N];
   
+  
+  float mPMin, mPMax;
+  float EPMin, EPMax;
+  float mC1Min, mC1Max;
+  float EC1Min, EC1Max;
+  float pC1Min, pC1Max;
+  float mCNewMin, mCNewMax;
+  float ECNewMin, ECNewMax;
+  float EG11Min, EG11Max;
+  float pG11Min, pG11Max;
+  float mTotalMin, mTotalMax;
+  
+ 
+  
+  //------------------------------------------------GENERATING P--------------------------------------------------------------------
+  
+  int i = 0;
+  while(i < N){
+    i++;
+  }
   
     
     
